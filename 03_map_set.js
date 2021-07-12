876eab37cff3ebc9229a4aeda14b439741cb1b3f
Valeriia <shapovalvv12@gmail.com>
2021-07-12 11:22:31 +0300

    fixed comments

--- 03_map_set.js
+++ 03_map_set.js
@@ -1,27 +1,16 @@
-const arr = [ { id: 1 }, { id: 2 }, { id: 3 }, { id: 1 }];
+const arr = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 1 }];
 
-let map = new Map;
-let set = new Set;
+let setOfId = new Set;
+let newArr = []
 
-function deleteDuplication (arr) {
-  for (let i = 0; i < arr.length; i++){
-    map.set(arr[i], arr[i].id)
+function deleteDuplication(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    setOfId.add(arr[i].id);
   }
-  
-  for (let value of map.values()) {
-    set.add(value)
-  }
-  
-  let newArr = [];
-  
-  for (value of set) {
-    for (let i = 0; i < arr.length; i++) {
-      if (value == arr[i].id) {
-        newArr.push(arr[i])
-        break;
-      }
-    }
-  }
-  return newArr;
+  let newArr = Array.from(setOfId, function (n) {
+    return { id: n }
+  });
+  return newArr
 }
+
 
